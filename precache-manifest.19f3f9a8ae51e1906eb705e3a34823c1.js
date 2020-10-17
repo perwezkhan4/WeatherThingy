85a3f64bb97a52e20c7387d307d92df0ac0c5b31
Perwez Khan <toperwez@gmail.com>
2020-10-18 00:48:22 +0530

    Updates

---
+++ precache-manifest.19f3f9a8ae51e1906eb705e3a34823c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f959c9b07b0b2f5963098de0f59a982",
+    "url": "/WeatherThingy/index.html"
+  },
+  {
+    "revision": "89381b17922a2c7c872c",
+    "url": "/WeatherThingy/static/css/main.327462c0.chunk.css"
+  },
+  {
+    "revision": "7acd7de30dda40ef979b",
+    "url": "/WeatherThingy/static/js/2.73e94717.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/WeatherThingy/static/js/2.73e94717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89381b17922a2c7c872c",
+    "url": "/WeatherThingy/static/js/main.6a66cf9f.chunk.js"
+  },
+  {
+    "revision": "9324b07afacf0c7d6bcd",
+    "url": "/WeatherThingy/static/js/runtime-main.b30fc220.js"
+  }
+]);
